dfd364b8b33d0f3c9791f604ac78bf728a02c1c0
Natalia Humenyk <gumenuknatalia53@gmail.com>
2026-06-13 22:21:58 +0300

    Solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,16 +1,7 @@
-type TodoListItem = {
-  id: number;
-  userId: number;
-  title: string;
-  completed: boolean;
-  user: {
-    name: string;
-    email: string;
-  };
-};
+import type { Todo } from '../../api/todos';
 
 type TodoListProps = {
-  todos: TodoListItem[];
+  todos: Todo[];
 };
 
 export const TodoList: React.FC<TodoListProps> = ({ todos }) => (
@@ -23,9 +14,7 @@ export const TodoList: React.FC<TodoListProps> = ({ todos }) => (
       >
         <td>{todo.id}</td>
         <h2 className="TodoInfo__title">{todo.title}</h2>
-        <a className="UserInfo" href={`mailto:${todo.user.email}`}>
-          {todo.user.name}
-        </a>
+        <p>User ID: {todo.userId}</p>
       </article>
     ))}
   </tbody>
